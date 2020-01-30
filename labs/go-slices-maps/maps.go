83ea67ef07cbb33ff62656854ef58a0f4af3b92f
Felipe Lopez Valdez <0204021@up.edu.mx>
2020-01-29 18:38:04 -0600

    Add maps homework
    Signed-off-by: Felipe Lopez Valdez <0204021@up.edu.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,34 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+	"fmt"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+
+	a := strings.Split(s, " ")
+	count := 0
+	fmt.Println(len(a))
+
+	for i := 0; i < len(a); i++{
+		count++
+		for j := 0; j < len(a); j++{
+			if i != j{
+				if a[i] == a[j]{
+					count++
+				}
+			}
+		}
+		m[a[i]] = count
+		count = 0
+	}
+	return m
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
