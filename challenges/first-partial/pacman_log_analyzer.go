983cad42e0444f140181a43e2f50f49075d2ffda
Felipe Lopez Valdez <0204021@up.edu.mx>
2020-02-16 14:37:20 -0600

    Add pacman_log_analyzer challenge
    Signed-off-by: Felipe Lopez Valdez <0204021@up.edu.mx>

--- challenges/first-partial/pacman_log_analyzer.go
+++ challenges/first-partial/pacman_log_analyzer.go
@@ -1,18 +1,85 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Pacman Log Analyzer")
+type File struct {
+	pckgName     string
+	installDate  string
+	lastUpdtDate string
+	nUpdt        int
+	removalDate  string
+}
 
-	if len(os.Args) < 2 {
-		fmt.Println("You must send at least one pacman log file to analize")
-		fmt.Println("usage: ./pacman_log_analizer <logfile>")
-		os.Exit(1)
+func main() {
+	file, err := os.Open("pacman.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	m := make(map[string]File)
+	upgCntr := 0
+	rmvCntr := 0
+	instllCntr := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		for i, s := range line {
+			if s == "installed" || s == "upgraded" || s == "removed" {
+				fileName := line[i+1]
+				fl := m[fileName]
+				fl.pckgName = fileName
+				if fl.removalDate == "" || fl.lastUpdtDate == "" {
+					fl.removalDate = "-"
+					fl.lastUpdtDate = "-"
+				}
+				if s == "installed" {
+					instllCntr = instllCntr + 1
+					fl.nUpdt = 0
+					fl.installDate = line[i-3] + " " + line[i-2]
+					fl.installDate = strings.Trim(fl.installDate, "[")
+					fl.installDate = strings.Trim(fl.installDate, "]")
+				}
+				if s == "upgraded" {
+					upgCntr = upgCntr + 1
+					fl.nUpdt = fl.nUpdt + 1
+					fl.lastUpdtDate = line[i-3] + " " + line[i-2]
+					fl.lastUpdtDate = strings.Trim(fl.lastUpdtDate, "[")
+					fl.lastUpdtDate = strings.Trim(fl.lastUpdtDate, "]")
+				}
+				if s == "removed" {
+					rmvCntr = rmvCntr + 1
+					fl.removalDate = line[i-3] + " " + line[i-2]
+					fl.removalDate = strings.Trim(fl.removalDate, "]")
+					fl.removalDate = strings.Trim(fl.removalDate, "[")
+				}
+				m[fileName] = fl
+			}
+		}
 	}
 
-	// Your fun starts here.
+	fmt.Println("Pacman Packages Report")
+	fmt.Println("----------------------")
+	fmt.Printf("- Installed files: %v\n", instllCntr)
+	fmt.Printf("- Removed files: %v\n", rmvCntr)
+	fmt.Printf("- Upgraded files: %v\n", upgCntr)
+	fmt.Printf("- Current installed: %v\n", instllCntr-rmvCntr)
+
+	for key, value := range m {
+		fmt.Println("List of packages")
+		fmt.Println("----------------")
+		fmt.Printf("- Package Name        : %v\n", key)
+		fmt.Printf("  - Install date      : %v\n", value.installDate)
+		fmt.Printf("  - Last update date  : %v\n", value.lastUpdtDate)
+		fmt.Printf("  - How many updates  : %v\n", value.nUpdt)
+		fmt.Printf("  - Removal date      : %v\n", value.removalDate)
+	}
 }
+
