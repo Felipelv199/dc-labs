cf804ae376c3c9a09f488eb049163e967565102e
Felipe Lopez Valdez <0204021@up.edu.mx>
2020-03-30 18:02:08 -0600

    Modifie ejercicio1.go
    Signed-off-by: Felipe Lopez Valdez <0204021@up.edu.mx>

--- labs/goroutines-vs-os-threads/ejercicio1.go
+++ labs/goroutines-vs-os-threads/ejercicio1.go
@@ -28,34 +28,3 @@ func main() {
 		f.WriteString(counterString)
 	}
 }
-package main
-
-import (
-	"os"
-	"strconv"
-)
-
-func gen(a, b chan int, c int) {
-	aux := <-a
-	b <- aux
-}
-
-func main() {
-	f, _ := os.Create("report1.txt")
-
-	defer f.Close()
-	// Set up the pipeline.
-	a := make(chan int)
-	b := make(chan int)
-	counter := 0
-	for {
-		go gen(b, a, counter)
-		a = b
-		b = make(chan int)
-		counter = counter + 1
-		counterString := strconv.Itoa(counter)
-		counterString = "\r" + counterString
-		f.WriteString(counterString)
-	}
-}
-
